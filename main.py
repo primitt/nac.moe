832a9e3c21211d012e2d9a2b34a1a14906643638
Priansh Mittra <81822069+primitt@users.noreply.github.com>
2025-01-11 18:14:27 -0800

    depdep

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, send_from_directory
 
 app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def index():
-    return render_template('comingsoon.html')
-
+    return render_template('index.html')
+@app.route('/media/<path>')
+def media(path):
+    return send_from_directory('media', path)
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
