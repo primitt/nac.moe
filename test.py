a90ee1cfb060005d46f495a1a9e5650bab417b3b
Priansh Mittra <81822069+primitt@users.noreply.github.com>
2025-03-15 12:44:31 -0700

    fix events and add event formatting

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 from db.db import database, events
 import datetime
 
+print(events.create(type="Meeting", name="Test Event", date=datetime.datetime.strptime("2025-03-10", "%Y-%m-%d"), time="12:00", location="Test Location", url="https://example.com"))
 print(events.create(type="Meeting", name="Test Event", date=datetime.datetime.strptime("2025-03-25", "%Y-%m-%d"), time="12:00", location="Test Location", url="https://example.com"))
-#events.delete().where(events.id == 1).execute()
+#events.delete().where(events.id < 20).execute()
